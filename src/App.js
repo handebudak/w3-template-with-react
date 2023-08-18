909c3585f0d2aef12de4246ddde0cccef92bd401
Hande Budak <bdkhande@gmail.com>
2023-08-18 16:29:30 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React from "react";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import Project_Section from "./components/Project_Section";
+import About_Section from "./components/About_Section";
+import Contact_Section from "./components/Contact_Section";
+import Footer1 from "./components/Footer1";
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+    <Navbar/>
+    <Header/>
+    <Project_Section/>
+    <About_Section/>
+    <Contact_Section/>
+    <Footer1/>
+ 
     </div>
   );
 }
